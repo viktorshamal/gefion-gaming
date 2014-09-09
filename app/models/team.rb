4e7484dd1cb591349ae73ae405101ccc232c4fb1
sphinxo <sphinxominator@gmmail.com>
2014-09-09 11:06:52 +0200

    tourn responsive

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,6 +3,8 @@ class Team < ActiveRecord::Base
   has_many :users, through: :teamrosters
   has_many :tournament_teams
 
+  validates_uniqueness_of :name
+
   def has_password?
     self.password ? @has_password = true : @has_password = false
   end
