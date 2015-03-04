89fe5c3bc0691c32032981fb1bc49fc371d54cf5
sphinxo <sphinxominator@gmail.com>
2015-03-04 12:50:58 +0100

    validate name and passwoRD;;;;;

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,7 +4,9 @@ class Team < ActiveRecord::Base
   has_many :tournament_teams
 
   validates_uniqueness_of :name
+  validates_format_of :name, :with => /^([^\d\W]|[-])*$/
   validates_length_of :name, :minimum => 3, :maximum => 15
+  validates_length_of :password, :minimum => 3, :maximum => 30
 
   def has_password?
     self.password ? @has_password = true : @has_password = false
