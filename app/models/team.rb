243a50c64803f0e9d485b8de69bce86abd93b035
sphinxo <sphinxominator@gmail.com>
2015-03-04 12:58:23 +0100

    unbroke everything

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,7 +4,7 @@ class Team < ActiveRecord::Base
   has_many :tournament_teams
 
   validates_uniqueness_of :name
-  validates_format_of :name, :with => /^([^\d\W]|[-])*$/
+  validates_format_of :name, with: /\A[a-zA-Z]+\z/
   validates_length_of :name, :minimum => 3, :maximum => 15
   validates_length_of :password, :minimum => 3, :maximum => 30
 
