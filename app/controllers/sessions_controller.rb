52118a0ef6c39dd8c20a488dccf9a1ad749e55e0
sphinxo <sphinxominator@gmail.com>
2015-03-11 17:25:28 +0100

    modals open from anchor now

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to root_url
+    redirect_to root_path(:anchor=> params[:state])
   end
 
   def destroy
